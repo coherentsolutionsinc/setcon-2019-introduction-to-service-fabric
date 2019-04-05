b64d7465618cac64bf01754bb0f2e43492533697
Oleg Karasik <OlegKarasik@coherentsolutions.com>
2019-04-05 16:26:11 +0300

    Added dynamic application name usage when creating remoting proxy to actor

--- src/JokesWeb/Clients/FabricJokesStatsClient.cs
+++ src/JokesWeb/Clients/FabricJokesStatsClient.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Fabric;
 using System.Threading;
 using System.Threading.Tasks;
 
 using JokesStats.Interfaces;
 
+using Microsoft.AspNetCore.Server.Kestrel.Core.Internal;
 using Microsoft.ServiceFabric.Actors;
 using Microsoft.ServiceFabric.Actors.Client;
 
@@ -11,6 +13,14 @@ namespace JokesWeb.Clients
 {
     public class FabricJokesStatsClient : IJokesStatsClient
     {
+        private readonly StatelessServiceContext context;
+
+        public FabricJokesStatsClient(
+            StatelessServiceContext context)
+        {
+            this.context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
         public async Task<JokesStatistics> IncrementViewedAndGetStatisticsAsync(
             string user,
             string language,
@@ -19,7 +29,7 @@ namespace JokesWeb.Clients
         {
             var actor = ActorProxy.Create<IJokesStats>(
                 new ActorId($"{user}/{language}/{category}"),
-                new Uri("fabric:/JokesApp/JokesStatsActorService"));
+                new Uri($"{this.context.CodePackageActivationContext.ApplicationName}/JokesStatsActorService"));
 
             await actor.IncrementViewed(cancellationToken);
 
